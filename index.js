94c0410aa493979dbba50ef16ada004dff51782c
Daniel <Daniel.duchatellier@gmail.com>
2021-04-07 11:21:05 -0400

    commit tree 0317a09bf82816cbebea8352a7ce331f93024315

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random()*10) +1;
+let num3 = 19;
+let num4 = 5;
+var mod = num3 % num4;
+
+let set = (1, 5, 15, 30, 20);
+var max = Math.max(set);
